e149e09689b36d4c0fcdc3f1b76755ad4bc29a0c
Dhaval Sojitra <143777610+dhaval-sojitra@users.noreply.github.com>
2024-02-13 13:07:04 +0530

    Add files via upload

---
+++ Java/result.java
@@ -0,0 +1,59 @@
+import java.util.*;
+class GET
+{
+	Scanner sc=new Scanner(System.in);
+	String sub[] = new String[5];
+	GET()
+	{
+		for(int i=0;i<sub.length;i++)
+		{	
+			System.out.print("Enter Subject "+(i+1)+" :");
+			sub[i] = sc.nextLine();
+			
+		}
+	}
+}
+class MARK extends GET
+{
+	int mark[] = new int[5];
+	MARK()
+	{
+		for(int i=0;i<mark.length;i++)
+		{
+			System.out.print("Enter Marks of "+sub[i]+" : ");
+			mark[i] = sc.nextInt();
+		}
+	}
+	void display()
+	{
+		int tot=0;
+		System.out.println("\t\t MCA SEM 2 RESULT\t\t");
+		System.out.println("\t------------------------------------------");
+		System.out.println("\tNo\tSubject\t\tMarks");
+		System.out.println("\t..........................................");
+		for(int i=0;i<mark.length;i++)
+		{
+			System.out.println("\t"+(i+1)+"\t"+sub[i]+"\t\t"+mark[i]);
+			tot = tot+mark[i];
+		}
+		System.out.println("\t------------------------------------------");
+		System.out.println("\t\tTotal : "+tot);
+		
+			if(mark[0]<50 || mark[1]<50 || mark[2]<50 || mark[3]<50 || mark[4]<50 )
+			{
+				System.out.println("\t\tPercentage : Fail");
+			} 
+			else
+			{
+				System.out.println("\t\tPecentage : "+tot/5);
+			}	
+	}
+
+}
+class result 
+{
+	public static void main(String[] args) {
+		MARK m = new MARK();
+		m.display(); 
+	}
+}
