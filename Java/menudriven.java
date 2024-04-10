9a11d3071fe88a91318bb9952d5e7a47991225da
Dhaval Sojitra <dhavalsojitra875@gmail.com>
2024-04-10 22:28:20 +0530

    sfdsf

--- Java/menudriven.java
+++ Java/menudriven.java
@@ -1,9 +1,8 @@
 import java.io.*;
 import java.util.*;
-class menu
-{
-	menu()
-	{
+
+class menu {
+	menu() {
 		System.out.println("------------------Choices------------------");
 		System.out.println("1.File Name");
 		System.out.println("2.File Path");
@@ -16,84 +15,153 @@ class menu
 		System.out.println("9.exit");
 	}
 }
-class getname{
-	getname()
-	{
+
+class getname {
+	getname() {
 		File f = new File("file1.txt");
 		String name = f.getName();
 		System.out.println("======================================");
-		System.out.println("File Name : "+name);
+		System.out.println("File Name : " + name);
 		System.out.println("======================================");
 
 	}
 }
-class getfilepath{
-	getfilepath()
-	{
+
+class getfilepath {
+	getfilepath() {
 		File f = new File("file1.txt");
 		String name = f.getAbsolutePath();
 		System.out.println("======================================");
-		System.out.println("File Path : "+name);
+		System.out.println("File Path : " + name);
 		System.out.println("======================================");
 
 	}
 }
-class canread{
-	canread(){
+
+class canread {
+	canread() {
 		File f = new File("file1.txt");
 		System.out.println("======================================");
-		System.out.println("File Can Be Read : "+f.canRead());
+		System.out.println("File Can Be Read : " + f.canRead());
 		System.out.println("======================================");
 
 	}
 }
-class canwrite{
-	canwrite()
-	{
+
+class canwrite {
+	canwrite() {
 		File f = new File("file1.txt");
 		System.out.println("======================================");
-		System.out.println("File Can Be Write : "+f.canWrite());
+		System.out.println("File Can Be Write : " + f.canWrite());
 		System.out.println("======================================");
 
-		
 	}
 }
-class select{
-	select(){
-	int i;
-	Scanner sc = new Scanner(System.in);
-	System.out.println("--------------------------------");
-	System.out.print("Enter Above choice : ");
-	i = sc.nextInt();
-	switch(i)
-	{
-	case 1:
-		new getname();
-		break;
-	case 2:
-		new getfilepath();
-		break;
-	case 3:
-		new canread();
-		break;
-	case 4:
-		new canwrite();
-		break;
-	case 9:
-		System.exit(0);
-	default:
-		System.out.println("Please Enter Valid Choice.");
+
+class write {
+	write() {
+		try {
+			FileWriter fw = new FileWriter("file1.txt");
+			fw.write("Is It enough?");
+			fw.close();
+			System.out.println("======================================");
+			System.err.println("File Wrote Successfully...");
+			System.out.println("======================================");
+		} catch (IOException e) {
+			System.err.println(e);
+		}
 	}
 }
-}
-class menudriven
-{
-	public static void main(String[] args) {
-		while(true)
+
+class read {
+	read(){
+		try{
+			FileReader fr = new FileReader("file1.txt");
+			int character;
+			System.out.println("======================================");
+			while((character=fr.read())!=-1)
+			{
+				System.err.print((char)character);
+			}
+			System.err.println();
+			System.out.println("======================================");
+		}
+		catch(IOException ie)
 		{
-		menu m = new menu();
-
-		select s = new select();
+			System.err.println(ie);
+		}
 	}
+}
+class rename{
+	rename()
+	{
+		File f = new File("file2.java");
+		String name = "File1.txt";
+		f.renameTo(new File(name));
+		System.out.println("======================================");
+		System.err.println("Rename file Successfully...");
+		System.out.println("======================================");
+
+	}
+}
+class delete{
+	delete(){
+		File f = new File("file1.txt");
+		f.delete();
+		System.out.println("======================================");
+		System.err.println("File Deleted Successfully...");
+		System.out.println("======================================");
+	}
+}
+class select {
+	select() {
+		int i;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("--------------------------------");
+		System.out.print("Enter Above choice : ");
+		i = sc.nextInt();
+		switch (i) {
+			case 1:
+				new getname();
+				break;
+			case 2:
+				new getfilepath();
+				break;
+			case 3:
+				new canread();
+				break;
+			case 4:
+				new canwrite();
+				break;
+			case 5:
+				new write();
+				break;
+			case 6:
+				new read();
+				break;
+			case 7:
+				new rename();
+				break;
+			case 8:
+				new delete();
+				break;
+			case 9:
+				System.exit(0);
+			default:
+				System.out.println("======================================");
+				System.out.println("Please Enter Valid Choice.");
+				System.out.println("======================================");
+
+		}
+	}
+}
+
+class menudriven {
+	public static void main(String[] args) {
+		while (true) {
+			menu m = new menu();
+
+			select s = new select();
+		}
 	}
 }
---
+++ Java/menudriven2.java
@@ -0,0 +1,204 @@
+import java.io.*;
+import java.util.*;
+
+class menu {
+    menu() {
+        System.err.println("--------------------Select Your Choice--------------------");
+        System.err.println("Enter 1 for Create New File :");
+        System.err.println("Enter 2 for get File Name :");
+        System.err.println("Enter 3 for get File Path :");
+        System.err.println("Enter 4 for know to file can read ? :");
+        System.err.println("Enter 5 for know to file can Write ? :");
+        System.err.println("Enter 6 for Write in File :");
+        System.err.println("Enter 7 for Read in File :");
+        System.err.println("Enter 8 for Rename to File :");
+        System.err.println("Enter 9 for Delete the File :");
+        System.err.println("Enter 10 for Exit :");
+        System.err.println("------------------------------------------------------------");
+    }
+}
+
+class create {
+    create() {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.err.print("Enter File Name :");
+            String name = sc.nextLine();
+            File f = new File(name);
+            f.createNewFile();
+            System.err.println("====================================================");
+            System.out.println("File Created Successfully...");
+            System.err.println("====================================================");
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
+
+class getname {
+    getname() {
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter File Path :");
+        String path = sc.nextLine();
+        File f = new File(path);
+        System.err.println("====================================================");
+        System.err.println("File Name :"+f.getName());
+        System.err.println("====================================================");
+    }
+}
+class getpath{
+    getpath(){
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter File Name :");
+        String name = sc.nextLine();
+        File f = new File(name);
+        System.err.println("====================================================");
+        System.err.println("File Path :"+f.getAbsolutePath());
+        System.err.println("====================================================");
+    }
+}
+class canread{
+    canread(){
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter File Name :");
+        String name = sc.nextLine();
+        File f = new File(name);
+        System.err.println("====================================================");
+        System.err.println("File Can Read :"+f.canRead());
+        System.err.println("====================================================");
+    }
+}
+class canwrite{
+    canwrite(){
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter File Name :");
+        String name = sc.nextLine();
+        File f = new File(name);
+        System.err.println("====================================================");
+        System.err.println("File Can Write :"+f.canWrite());
+        System.err.println("====================================================");
+    }
+}
+class write{
+    write(){
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter File Name :");
+        String name = sc.nextLine();
+        try{
+            FileWriter fw = new FileWriter(name);
+            System.err.print("Enter Content Which you enter in file :");
+            String data = sc.nextLine();
+            fw.write(data);
+            fw.close();
+            System.err.println("====================================================");
+            System.err.println("Write Content in file Successfully..");
+            System.err.println("====================================================");    
+        }
+        catch(IOException e){
+            System.err.println(e);
+        }
+    }
+}
+class read{
+    read(){
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter File Name :");
+        String name = sc.nextLine();
+        try{
+            FileReader fr = new FileReader(name);
+            int val;
+            System.err.println("====================================================");    
+            while((val=fr.read())!=-1)
+            {
+                System.err.print((char)val);
+            }
+            System.err.println();
+            System.err.println("====================================================");    
+            fr.close();
+        }
+        catch(IOException e){
+            System.err.println(e);
+        }
+    }
+}
+class rename{
+    rename(){
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter Exits File Name :");
+        String name = sc.nextLine();
+        System.out.print("Enter New Name :");
+        String newname = sc.nextLine();
+        File f = new File(name);
+        f.renameTo(new File(newname));
+        System.err.println("====================================================");
+        System.err.println("Renamed file Successfully..");
+        System.err.println("====================================================");    
+
+    }
+}
+class delete{
+    delete(){
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter Exits File Name :");
+        String name = sc.nextLine();
+        File f = new File(name);
+        f.delete();
+        System.err.println("====================================================");
+        System.err.println("File Deleted Successfully..");
+        System.err.println("====================================================");    
+
+    }
+}
+
+class select {
+    select() {
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter Your Choice :");
+        int i = sc.nextInt();
+        System.err.println("------------------------------------------------------------");
+        System.err.println();
+        switch (i) {
+            case 1:
+                new create();
+                break;
+            case 2:
+                new getname();
+                break;
+            case 3:
+                new getpath();
+                break;
+            case 4:
+                new canread();
+                break;
+            case 5:
+                new canwrite();
+                break;
+            case 6:
+                new write();
+                break;
+            case 7:
+                new read();
+                break;
+            case 8:
+                new rename();
+                break;
+            case 9:
+                new delete();
+                break;
+            case 10:
+                System.exit(0);
+            default:
+                System.err.println("Please Enter Valid Choice..");
+                break;
+        }
+    }
+}
+
+class menudriven2 {
+    public static void main(String[] args) {
+        while (true) {
+            menu m = new menu();
+            select s = new select();
+        }
+    }
+}
