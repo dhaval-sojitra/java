af97e693d906bfd5f2993a6e7342c0f6f19a41db
Dhaval Sojitra <dhavalsojitra875@gmail.com>
2024-04-14 18:33:20 +0530

    dfsf

--- Java/PractiseSet/Vowel.java
+++ Java/PractiseSet/Vowel.java
@@ -9,6 +9,60 @@
 // Are 2
 // Wonderful 3
 // Times 2
-class Vowel{
-
+import java.util.*;
+class chk{
+    Scanner sc = new Scanner(System.in);
+    chk(){
+        System.err.print("Enter a Stirng :");
+        String str=sc.nextLine();
+        System.err.println("------------------------------------");
+        System.err.println("String :"+str);
+        System.err.println("------------------------------------");
+        String substr[] = str.split(" ");
+        System.err.println("Word\t\tVowel");
+        for(String word : substr)
+        {
+            int count = 0;
+            System.err.print(word+"\t\t");
+            for(int i=0;i<word.length();i++)
+            {
+                char j = word.toLowerCase().charAt(i);
+                if(j=='a' || j=='e' || j=='i' || j=='o' || j=='u')
+                {
+                    count++;
+                }
+            }
+            System.err.println(count);
+        }
+    }
 }
+class Vowel{
+    public static void main(String[] args) {
+        chk c = new chk();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
