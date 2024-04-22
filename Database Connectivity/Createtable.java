9108556e0b5a26a1a021bba3448566f785d3fb3b
Dhaval Sojitra <dhavalsojitra875@gmail.com>
2024-04-22 23:05:40 +0530

    dfdsf

---
+++ Database Connectivity/Createtable.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+class Createtable{
+    public static void main(String[] args) {
+        try{
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java", "root", "");
+
+            String craetequery = "create table student(id int(4) primary key auto_increment,name varchar(255),city varchar(255),phone int(10))";
+
+            Statement st = con.createStatement();
+            st.executeUpdate(craetequery);
+
+            con.close();
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
--- Database Connectivity/_Con.java
+++ Database Connectivity/_Con.java
@@ -1,9 +1,9 @@
 import java.sql.*;
-class Con{
+class _Con{
 	public static void main(String[] args) {
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url = "jdbc:mysql://localhost:3306/dhaval";
+			String url = "jdbc:mysql://localhost:3306/myblog";
 			String uname = "root";
 			String password = "";
 			Connection con = DriverManager.getConnection(url,uname,password);
