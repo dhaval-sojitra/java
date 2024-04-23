5a0642a665f78bc27d4e88be45512102ed167ec7
unknown <dhavalsojitra875@gmail.com>
2024-04-23 12:23:43 +0530

    sdfe

--- Database Connectivity/Con1.java
+++ Database Connectivity/Con1.java
@@ -3,7 +3,7 @@ public class Con1 {
     public static void main(String[] args) {
         try{
 
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/myblog", "root", "");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dhaval", "root", "");
             if(con.isClosed())
             {
                 System.err.println("Database Not Connected Successfully...");
